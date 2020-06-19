c6dd7dfdc681bb0bc00de73b6a22989e23519dd0
Massimo Meneghello <massimo.meneghello93@gmail.com>
2020-06-19 02:28:08 +0100

    print matrix function added

--- src/common.cpp
+++ src/common.cpp
@@ -1,2 +1,31 @@
 
 #include "common.h"
+
+int utils_printDmat(int m, int n, int l, double *mat, bool device)
+{
+    double *matLoc;
+    if (device)
+    {
+        matLoc = (double *)malloc(sizeof(double) * l * n);
+        checkCudaErrors(cudaMemcpy(matLoc, mat, sizeof(double) * l * n, cudaMemcpyDeviceToHost));
+    } else {
+        matLoc = mat;
+    }
+    
+    for (int i = 0; i < l; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            printf("%6.2lf ", matLoc[l * i + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    if (device)
+    {
+        free(matLoc);
+    }
+    
+    return 0;
+}
